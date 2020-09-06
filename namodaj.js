9e0f62f0b513cb49c32c8ca9eabd3357013e5db0
semsemlockscreen <70825069+semsemlockscreen@users.noreply.github.com>
2020-09-06 13:09:21 +0100

    Update namodaj.js

--- namodaj.js
+++ namodaj.js
@@ -12131,7 +12131,7 @@ p.nominalBounds = new cjs.Rectangle(-425.3,-76.7,850.6,144.3);
 
 //}, 100);
                       stage.on("drawstart", initTextStyle, this, true);
-		
+		alert('initTextStyle0');
 		function initTextStyle() {
 		
 			document.addEventListener('keyup', function (e) {
