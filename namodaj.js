8fc3798b1895bf2ec2181395270947297ec25164
semsemlockscreen <70825069+semsemlockscreen@users.noreply.github.com>
2020-09-06 21:34:00 +0100

    Update namodaj.js

--- namodaj.js
+++ namodaj.js
@@ -12399,7 +12399,7 @@ stage.on("drawstart", initTextStyle, this, true);
 		
 		
 		function toNx(ev) {
-			
+			stage.preventSelection = false;
 			if (ev.nativeEvent instanceof MouseEvent) {
 		
 				if (currentPage + 2 > maxPage) return;
